9f061d9ea4110c53fa39e2fbfc92d29a67a3ff0c
jodisaputra <116236799+jodisaputra@users.noreply.github.com>
2024-05-29 15:37:50 +0700

    Setup Front End

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', \App\Livewire\HomePage::class);
+Route::get('/categories', \App\Livewire\CategoriesPage::class);
+Route::get('/products',\App\Livewire\ProductsPage::class);
+Route::get('/cart',\App\Livewire\CartPage::class);
+Route::get('/products/{product}',\App\Livewire\ProductDetailPage::class)->name('product.show');
+Route::get('/checkout',\App\Livewire\CheckoutPage::class);
+Route::get('/my-orders',\App\Livewire\MyOrderPage::class);
+Route::get('/my-orders/{order}',\App\Livewire\MyOrderDetailPage::class);
+
+
+Route::get('/login',\App\Livewire\Auth\LoginPage::class);
+Route::get('/register',\App\Livewire\Auth\RegisterPage::class);
+Route::get('/forgot',\App\Livewire\Auth\ForgotPasswordPage::class);
+Route::get('/reset',\App\Livewire\Auth\ResetPasswordPage::class);
+
+Route::get('/success', \App\Livewire\SuccessPage::class);
+Route::get('/cancel', \App\Livewire\CancelPage::class);
