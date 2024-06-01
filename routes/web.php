8781be84b6d204fd2bfff0f2e85fc2eb5c8872ad
jodisaputra <116236799+jodisaputra@users.noreply.github.com>
2024-06-01 15:07:03 +0700

    Login & Register

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,18 +16,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', \App\Livewire\HomePage::class);
 Route::get('/categories', \App\Livewire\CategoriesPage::class);
-Route::get('/products',\App\Livewire\ProductsPage::class);
-Route::get('/cart',\App\Livewire\CartPage::class);
-Route::get('/products/{slug}',\App\Livewire\ProductDetailPage::class)->name('product.show');
-Route::get('/checkout',\App\Livewire\CheckoutPage::class);
-Route::get('/my-orders',\App\Livewire\MyOrderPage::class);
-Route::get('/my-orders/{order}',\App\Livewire\MyOrderDetailPage::class);
+Route::get('/products', \App\Livewire\ProductsPage::class);
+Route::get('/cart', \App\Livewire\CartPage::class);
+Route::get('/products/{slug}', \App\Livewire\ProductDetailPage::class)->name('product.show');
 
+Route::middleware('guest')->group(function () {
+    Route::get('/login', \App\Livewire\Auth\LoginPage::class)->name('login');
+    Route::get('/register', \App\Livewire\Auth\RegisterPage::class);
+    Route::get('/forgot', \App\Livewire\Auth\ForgotPasswordPage::class);
+    Route::get('/reset', \App\Livewire\Auth\ResetPasswordPage::class);
+});
 
-Route::get('/login',\App\Livewire\Auth\LoginPage::class);
-Route::get('/register',\App\Livewire\Auth\RegisterPage::class);
-Route::get('/forgot',\App\Livewire\Auth\ForgotPasswordPage::class);
-Route::get('/reset',\App\Livewire\Auth\ResetPasswordPage::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/logout', function() {
+        Auth::logout();
+        return redirect('/');
+    });
+    Route::get('/checkout', \App\Livewire\CheckoutPage::class);
+    Route::get('/my-orders', \App\Livewire\MyOrderPage::class);
+    Route::get('/my-orders/{order}', \App\Livewire\MyOrderDetailPage::class);
 
-Route::get('/success', \App\Livewire\SuccessPage::class);
-Route::get('/cancel', \App\Livewire\CancelPage::class);
+    Route::get('/success', \App\Livewire\SuccessPage::class);
+    Route::get('/cancel', \App\Livewire\CancelPage::class);
+});
